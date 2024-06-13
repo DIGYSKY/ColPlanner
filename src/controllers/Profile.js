8e478302910bd4542d6571deb213d0ea9a271400
Lilyan Chauveau <lilyan.chauveau@gmail.com>
2024-06-13 22:39:38 +0200

    add function (update) => user

--- src/controllers/Profile.js
+++ src/controllers/Profile.js
@@ -4,18 +4,18 @@ import coockieManager from '../models/cookieManager';
 
 const Profile = class {
   constructor(id) {
-    this.el = document.querySelector('#root');
     this.apiLinks = 'http://localhost:81';
     this.apiKey = coockieManager.getCookie('apikey');
     this.user = JSON.parse(coockieManager.getCookie('user'));
     this.currentColoc = this.user.current_coloc ? this.user.current_coloc : false;
+    this.eventListeners = [];
     this.run(id);
   }
 
   logout() {
     const logoutButton = document.querySelector('.logout-profile');
 
-    logoutButton.addEventListener('click', async (event) => {
+    this.addEventListeners([logoutButton], 'click', async (event) => {
       event.preventDefault();
 
       try {
@@ -63,9 +63,118 @@ const Profile = class {
     }
   }
 
+  actionsProfile() {
+    this.addEventListeners([document.querySelector('.update-profile')], 'submit', (event) => {
+      event.preventDefault();
+      this.handleUpdateProfile();
+    });
+  }
+
+  async handleUpdateProfile() {
+    const name = document.querySelector('.name').value;
+    const email = document.querySelector('.email').value;
+    const password = document.querySelector('.password').value;
+    const confirmPassword = document.querySelector('.confirm-password').value;
+
+    let response = false;
+    let passwordResponse = false;
+
+    if (name && name !== '' && name !== this.user.name) {
+      response = await this.updateName(name);
+    }
+
+    if (email && email !== '' && email !== this.user.email) {
+      response = await this.updateEmail(email);
+    }
+
+    if (
+      password
+      && password !== ''
+      && password !== this.user.password
+      && confirmPassword
+      && confirmPassword !== ''
+      && confirmPassword === password
+    ) {
+      passwordResponse = await this.updatePassword(password, confirmPassword);
+    }
+
+    if (response) {
+      this.user = response;
+      coockieManager.setCookie('user', JSON.stringify(this.user));
+    }
+
+    if (passwordResponse === false && confirmPassword !== '') {
+      document.querySelectorAll('.password-error').forEach((element) => {
+        element.classList.remove('hidden');
+      });
+    } else {
+      document.querySelectorAll('.password-error').forEach((element) => {
+        element.classList.add('hidden');
+      });
+    }
+  }
+
+  async updateName(name) {
+    try {
+      const response = await axios.patch(`${this.apiLinks}/user/edit/name/${this.user.id}`, {
+        name
+      }, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+
+      return response.data.success;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async updateEmail(email) {
+    try {
+      const response = await axios.patch(`${this.apiLinks}/user/edit/email/${this.user.id}`, {
+        email
+      }, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+
+      return response.data.success;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async updatePassword(password, confirmPassword) {
+    try {
+      const response = await axios.patch(`${this.apiLinks}/user/edit/password/${this.user.id}`, {
+        password,
+        confirmPassword
+      }, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+
+      return response.data.success;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  addEventListeners(elements, event, callback) {
+    elements.forEach((element) => {
+      element.addEventListener(event, callback);
+      this.eventListeners.push({ element, event, callback });
+    });
+  }
+
   run(id) {
     this.renderProfile(id);
     this.logout();
+    this.actionsProfile();
+  }
+
+  destroy() {
+    this.eventListeners.forEach(({ element, event, callback }) => {
+      element.removeEventListener(event, callback);
+    });
+    this.eventListeners = [];
   }
 };
 
--- src/views/app/profile.js
+++ src/views/app/profile.js
@@ -1,30 +1,32 @@
-export default () => (`
+export default (user) => (`
 <h2 class="mx-auto mt-5 mb-5">Profile</h2>
 <div class="mx-auto mt-0 flex justify-center align-center gap-5 flex-wrap">
 
 <!-- Card pour modifier le nom, l'email et le mot de passe -->
 <div class="bg-white p-6 rounded shadow w-full sm:w-4/5">
   <h3 class="text-xl font-semibold mb-4 text-yellow-500">Modifier le profil</h3>
-  <form id="update-profile-form" class="space-y-4">
+  <form class="update-profile space-y-4">
     <div>
       <label for="name" class="block text-sm font-medium text-gray-700">Nom</label>
-      <input type="text" id="name" name="name"
-        class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+      <input type="text" name="name" value="${user.name}"
+        class="name mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
     </div>
     <div>
       <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-      <input type="email" id="email" name="email"
-        class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+      <input type="email" name="email" value="${user.email}"
+        class="email mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
     </div>
     <div>
       <label for="password" class="block text-sm font-medium text-gray-700">Mot de passe</label>
-      <input type="password" id="password" name="password"
-        class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+      <input type="password" name="password"
+        class="password mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+      <p class="password-error hidden text-red-500">Mot de passe incorrecte</p>
     </div>
     <div>
       <label for="confirm-password" class="block text-sm font-medium text-gray-700">Confirmer le mot de passe</label>
-      <input type="password" id="confirm-password" name="confirm-password"
-        class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+      <input type="password" name="confirm-password"
+        class="confirm-password mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+      <p class="password-error hidden text-red-500">Mot de passe incorrecte</p>
     </div>
     <button type="submit" class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded">Mettre à jour</button>
   </form>
