65be037313edef1ed91a02e14da4f712473b7f7a
Lilyan Chauveau <lilyan.chauveau@gmail.com>
2024-06-13 19:48:23 +0200

    add dynamics view finance and add function (pay, delete) => funance

--- src/controllers/Finance.js
+++ src/controllers/Finance.js
@@ -1,23 +1,66 @@
+import axios from 'axios';
 import finance from '../views/app/finance';
 import coockieManager from '../models/cookieManager';
 
 const Finance = class {
   constructor(id) {
-    this.el = document.querySelector('#root');
     this.apiLinks = 'http://localhost:81';
     this.apiKey = coockieManager.getCookie('apikey');
     this.user = JSON.parse(coockieManager.getCookie('user'));
-    this.currentColoc = this.user.current_coloc ? this.user.current_coloc : false;
+    this.currentColoc = coockieManager.getCookie('currentColoc') ?? false;
+    this.eventListeners = [];
     this.run(id);
+  }
+
+  async getCurrentColoc() {
+    let request;
+    try {
+      request = await axios.get(`${this.apiLinks}/user/coloc/current/this`, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+    } catch (error) {
+      return false;
+    }
+    const currentColoc = request.data.success.current_coloc;
+    coockieManager.setCookie('current_coloc', currentColoc, 7);
+    return currentColoc;
+  }
+
+  async getLog() {
+    if (!this.apiKey) return false;
+    try {
+      await axios.get(`${this.apiLinks}/login`, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+      return true;
+    } catch (error) {
+      this.handleLogout();
+      return false;
+    }
+  }
+
+  handleLogout() {
+    coockieManager.deleteCookie('apikey');
+    coockieManager.deleteCookie('user');
+    this.apiKey = false;
+    localStorage.removeItem('page');
+    window.location.href = '/login';
+  }
+
+  async getExpenceOwe() {
+    const request = await axios.get(`${this.apiLinks}/expence/this`, {
+      headers: { 'Api-Key': this.apiKey }
+    });
+    return request.data.success;
   }
 
   async renderFinance(id) {
     const mainButtons = Array.from(document.querySelectorAll('.main-col'));
     const main = document.querySelector('#app-corp');
-
     let select = false;
+    const expenceOwe = await this.getExpenceOwe();
 
-    main.innerHTML = finance(this.user);
+    main.innerHTML = finance(expenceOwe, this.user.id);
 
     if (main.innerHTML) {
       select = true;
@@ -35,8 +78,96 @@ const Finance = class {
     }
   }
 
-  run(id) {
-    this.renderFinance(id);
+  actionExpence() {
+    this.addEventListeners([document.querySelector('.send-expence')], 'submit', this.actionSendExpence.bind(this));
+    this.addEventListeners(document.querySelectorAll('.delete-expence'), 'click', this.actionDeleteExpence.bind(this));
+    this.addEventListeners(document.querySelectorAll('.pay-expence'), 'click', this.actionPayExpence.bind(this));
+  }
+
+  async actionPayExpence(e) {
+    const id = e.target.dataset.idPay;
+    try {
+      const request = await axios.patch(`${this.apiLinks}/expence/${id}`, {
+        paid: true
+      }, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+
+      if (request.data.success) {
+        this.run();
+      } else {
+        e.target.classList.add('error');
+      }
+    } catch (error) {
+      window.location.reload();
+    }
+  }
+
+  actionSendExpence(e) {
+    e.preventDefault();
+    const expence = e.target.querySelector('input[name="expence"]').value.trim();
+    const price = e.target.querySelector('input[name="price"]').value.trim();
+    const description = e.target.querySelector('textarea[name="description"]').value.trim();
+
+    if (expence === '' || price === '' || description === '') {
+      return;
+    }
+
+    const request = this.postExpence(expence, price, description);
+    if (request) {
+      this.run();
+    }
+  }
+
+  async actionDeleteExpence(e) {
+    const id = e.target.dataset.idDelete;
+    const request = await axios.delete(`${this.apiLinks}/expence/${id}`, {
+      headers: { 'Api-Key': this.apiKey }
+    });
+    if (request.data.success) {
+      this.run();
+    }
+  }
+
+  async postExpence(expence, price, description) {
+    try {
+      const request = await axios.post(`${this.apiLinks}/expence/this`, {
+        expence,
+        price,
+        description
+      }, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+      return request.data.success;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  addEventListeners(elements, event, callback) {
+    elements.forEach((element) => {
+      if (element) {
+        element.addEventListener(event, callback);
+        this.eventListeners.push({ element, event, callback });
+      }
+    });
+  }
+
+  async run(id = 'finance-col') {
+    if (this.currentColoc && (await this.getLog())) {
+      await this.renderFinance(id);
+      this.actionExpence();
+    } else {
+      this.currentColoc = await this.getCurrentColoc();
+      this.run();
+    }
+  }
+
+  destroy() {
+    this.eventListeners.forEach(({ element, event, callback }) => {
+      element.removeEventListener(event, callback);
+    });
+    this.eventListeners = [];
   }
 };
 
--- src/views/app/finance.js
+++ src/views/app/finance.js
@@ -1,59 +1,66 @@
-export default () => (`
-<div class="container-financecol">
-<div class="solde-financecol">
-  <p class="sold-title-financecol">Your solde -35 €</p>
-</div>
-<div class="you-owe-financecol">
-  <p class="you-owe-title-financecol">You owe to</p>
-  <div class="you-owe-content-financecol">
-    <div class="line">
-      <p class="who">Robert</p>
-      <button class="pay button">Pay</button>
-      <p class="mutch">35 €</p>
-    </div>
+export default (expences, userId) => `
+  <div class="col-span-2">
+    <p class="text-2xl font-bold text-yellow-500">Votre solde ${expences.solds[userId].sold} €</p>
   </div>
-</div>
-<div class="last-expence-financecol">
-  <p class="last-expence-title-financecol">Last expence</p>
-  <div class="last-expence-content-financecol">
-    <div class="line">
-      <p class="title">Course</p>
-      <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ipsam.</p>
-      <p class="price">122 €</p>
-      <p class="who">By Alice</p>
-      <div class="edit">
-        <button class="button delete">Delete</button>
-        <button class="button edit-">Édit</button>
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 w-full min-h-full">
+    <!-- Vous devez à -->
+    <div class="bg-white p-6 rounded shadow w-full">
+      <p class="text-xl font-semibold mb-4 text-yellow-500">Vous devez à</p>
+      <div class="overflow-auto">
+        ${expences.owes.length > 0 ? expences.owes.map((owe) => (owe.from === userId ? `
+          <div class="flex justify-between items-center bg-gray-200 p-4 rounded text-black font-bold mb-1">
+            <p>${owe.name_to}</p>
+            <p>${owe.amount} €</p>
+          </div>
+        ` : '')).join('') : '<p class="text-gray-600">Vous ne devez pas d\'argent</p>'}
       </div>
     </div>
-  </div>
-</div>
-<div class="owe-you-financecol">
-  <p class="owe-you-title-financecol">Owe you to</p>
-  <div class="owe-you-content-financecol">
-    <div class="line">
-      <p class="who">Alice</p>
-      <button class="pay button">Pay</button>
-      <p class="mutch">10 €</p>
+
+    <!-- Dernière dépense -->
+    <div class="bg-white p-6 rounded shadow w-full">
+      <p class="text-xl font-semibold mb-4 text-yellow-500">Dernière dépense</p>
+      <div class="overflow-auto">
+        ${expences.expences.length > 0 ? expences.expences.map((expence) => `
+          <div class="flex flex-col items-center bg-gray-200 p-4 rounded text-black font-bold mb-4">
+            <p class="text-red-500 mb-1 text-center text-2xl">${expence.name}</p>
+            <div class="flex justify-between w-full items-center">
+              <p class="text-black w-1/3 text-center">${expence.description}</p>
+              <p class="text-blue-500 w-1/3 text-center">${expence.price} €</p>
+              <p class="w-1/3 text-center">Par ${expence.user_name}</p>
+            </div>
+            <div class="flex justify-center mt-4 w-full">
+              <button class="delete-expence bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 mr-2" data-id-delete="${expence.id}">Supprimer</button>
+              ${expence.user_id !== userId && (expences.expencesPay.userPay[expence.id] ? !expences.expencesPay.userPay[expence.id].includes(userId) : true) ? `<button class="pay-expence bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-2" data-id-pay="${expence.id}">Payer</button>` : ''}
+            </div>
+          </div>
+        `).join('') : '<p class="text-gray-600">Aucune dépense récente</p>'}
+      </div>
+    </div>
+
+    <!-- On vous doit -->
+    <div class="bg-white p-6 rounded shadow w-full">
+      <p class="text-xl font-semibold mb-4 text-yellow-500">On vous doit</p>
+      <div class="overflow-auto">
+        ${expences.owes.length > 0 ? expences.owes.map((owe) => (owe.to === userId ? `
+          <div class="flex justify-between items-center bg-gray-200 p-4 rounded text-black font-bold mb-1">
+            <p>${owe.name_from}</p>
+            <p>${owe.amount} €</p>
+          </div>
+        ` : '')).join('') : '<p class="text-gray-600">Personne ne vous dois d\'argent</p>'}
+      </div>
+    </div>
+
+    <!-- Ajouter une dépense -->
+    <div class="bg-white p-6 rounded shadow w-full">
+      <p class="text-xl font-semibold mb-4 text-yellow-500">Ajouter une dépense</p>
+      <form class="send-expence space-y-4">
+        <div class="flex gap-4 items-center">
+          <input type="text" name="expence" placeholder="Dépense" class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+          <input type="text" name="price" placeholder="Prix" class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+        </div>
+        <textarea name="description" placeholder="Description" class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black resize-vertical"></textarea>
+        <input type="submit" value="Soumettre" class="mt-2 p-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600">
+      </form>
     </div>
   </div>
-</div>
-<div class="add-expence-financecol">
-  <p class="add-expence-title-financecol">Add expence</p>
-  <form>
-    <div class="input-container">
-      <label for="expence">Expence</label>
-      <input type="text" name="expence" placeholder="Expence">
-
-      <label for="price">Price</label>
-      <input type="text" name="price" placeholder="Price">
-    </div>
-    <label for="description">Description</label>
-    <textarea name="description" placeholder="Description"></textarea>
-
-    <input type="submit" value="Submit">
-  </form>
-
-</div>
-</div>
-`);
+`;
