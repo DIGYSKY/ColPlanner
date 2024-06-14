6e4a25d5f450233df1e66c3795005f3b4d802554
Lilyan Chauveau <lilyan.chauveau@gmail.com>
2024-06-14 13:26:20 +0200

    update style finance and add function (getTasks, patchTask, deleteTasks) => Finance and update function (renderFinance) => Finance for finance view

--- src/controllers/Tasks.js
+++ src/controllers/Tasks.js
@@ -1,23 +1,36 @@
+import axios from 'axios';
 import tasks from '../views/app/tasks';
 import coockieManager from '../models/cookieManager';
 
 const Tasks = class {
   constructor(id) {
-    this.el = document.querySelector('#root');
     this.apiLinks = 'http://localhost:81';
     this.apiKey = coockieManager.getCookie('apikey');
     this.user = JSON.parse(coockieManager.getCookie('user'));
     this.currentColoc = this.user.current_coloc ? this.user.current_coloc : false;
+    this.eventListeners = [];
     this.run(id);
+  }
+
+  async getTasks() {
+    try {
+      const request = await axios.get(`${this.apiLinks}/task/this`, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+      return request.data;
+    } catch (error) {
+      return [];
+    }
   }
 
   async renderTasks(id) {
     const mainButtons = Array.from(document.querySelectorAll('.main-col'));
     const main = document.querySelector('#app-corp');
+    const getTasks = await this.getTasks();
 
     let select = false;
 
-    main.innerHTML = tasks(this.user);
+    main.innerHTML = tasks(getTasks);
 
     if (main.innerHTML) {
       select = true;
@@ -35,8 +48,84 @@ const Tasks = class {
     }
   }
 
-  run(id) {
-    this.renderTasks(id);
+  actionsTasks() {
+    this.addEventListeners([document.querySelector('.add-task-form')], 'submit', (e) => {
+      e.preventDefault();
+      const title = e.target.querySelector('[name="title"]').value;
+      const content = e.target.querySelector('[name="content"]').value;
+      const date = e.target.querySelector('[name="date"]').value || null;
+      if (title !== '' && content !== '') {
+        if (this.sendTask(title, content, date)) {
+          this.run();
+        }
+      }
+    });
+    this.addEventListeners([document.querySelector('.make-task')], 'click', async (e) => {
+      e.preventDefault();
+      const { id, make } = e.target.dataset;
+      if (await this.patchTask(id, make)) {
+        this.run();
+      }
+    });
+    this.addEventListeners([document.querySelector('.delete-task')], 'click', async (e) => {
+      e.preventDefault();
+      const { id } = e.target.dataset;
+      if (await this.deleteTask(id)) {
+        this.run();
+      }
+    });
+  }
+
+  async patchTask(id, make) {
+    const request = await axios.patch(`${this.apiLinks}/task/${id}`, {
+      make
+    }, {
+      headers: { 'Api-Key': this.apiKey }
+    });
+    return request.data;
+  }
+
+  async deleteTask(id) {
+    const request = await axios.delete(`${this.apiLinks}/task/${id}`, {
+      headers: { 'Api-Key': this.apiKey }
+    });
+    return request.data;
+  }
+
+  async sendTask(title, content, date) {
+    try {
+      const request = await axios.post(`${this.apiLinks}/task/new`, {
+        title,
+        content,
+        date
+      }, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+      return request.data;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  addEventListeners(elements, event, callback) {
+    elements.forEach((element) => {
+      if (element) {
+        element.addEventListener(event, callback);
+        this.eventListeners.push({ element, event, callback });
+      }
+    });
+  }
+
+  async run(id = 'tasks-col') {
+    await this.renderTasks(id);
+    this.actionsTasks();
+  }
+
+  destroy() {
+    this.eventListeners.forEach(({ element, event, callback }) => {
+      element.removeEventListener(event, callback);
+    });
+    this.eventListeners = [];
   }
 };
 
--- src/views/app/tasks.js
+++ src/views/app/tasks.js
@@ -1,45 +1,54 @@
-export default () => (`
-<h2 class="mx-auto mt-5 mb-5">Tasks</h2>
-<div class="mx-auto mt-0 flex justify-center align-center gap-5 flex-wrap">
+export default (getTasks) => (`
+<h2 class="mx-auto mt-5 mb-5 text-2xl font-bold text-yellow-500">Tasks</h2>
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 w-full min-h-full">
 
 <!-- Card pour afficher toutes les tâches -->
-<div class="card-col w-full sm:w-1/2">
-  <h3 class="card-title-col">Tâches</h3>
-  <div class="card-content-col">
-    <ul id="task-list" class="space-y-2 h-64 overflow-auto">
-      <li class="flex flex-col bg-gray-200 p-4 rounded text-black font-bold">
-        <span class="text-lg text-red-600">Faire les courses</span>
-        <span class="text-sm text-gray-600">Acheter du lait, des œufs et du pain</span>
-        <div class="flex flex-col items-left mt-2">
-          <span class="text-sm text-green-600">À faire avant le 20/10/2023</span>
-          <span class="text-sm text-black">Fait le 10 mai 2024</span>
+<div class="card-display-tasks bg-white p-6 rounded shadow w-full">
+  <h3 class="text-xl font-semibold mb-4 text-yellow-500">Tâches</h3>
+  <div class="overflow-auto h-full">
+    <ul class="task-list space-y-2 h-full overflow-auto">
+      ${getTasks.length > 0 ? getTasks.map((task) => `
+      <li class="flex flex-col items-center bg-gray-200 p-4 rounded text-black font-bold mb-4">
+        <span class="text-lg text-red-600">${task.title}</span>
+        <span class="text-sm text-gray-600">${task.content}</span>
+        <div class="flex flex-col items-center mt-2">
+          ${task.at_before !== null ? `<span class="text-sm text-green-600">À faire avant le ${task.at_before}</span>` : ''}
+          <span class="text-sm text-black">Créer le ${task.created_at} par ${task.user_name}</span>
         </div>
-        <div class="flex justify-end mt-2">
-          <button class="bg-red-600 text-white px-4 py-2 rounded">Fait</button>
+        <div class="flex justify-center mt-4 w-full space-x-2">
+  ${task.make === 0
+    ? `   <button class="make-task bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700" data-make="1" data-id="${task.id}">Fait</button>`
+    : `   <button class="make-task bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700" data-make="0" data-id="${task.id}">Non fait</button>`
+  }
+          <button class="delete-task bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700" data-id="${task.id}">Supprimer</button>
         </div>
       </li>
+      `).join('') : '<li class="text-center text-gray-500">Aucune tâche disponible</li>'}
     </ul>
   </div>
 </div>
 
 <!-- Card pour ajouter des tâches -->
-<div class="card-col w-full sm:w-1/2">
-  <h3 class="card-title-col">Ajouter une tâche</h3>
-  <div class="card-content-col p-8">
-    <form id="add-task-form" class="space-y-4">
-      <div>
-        <label for="task" class="block text-sm font-medium text-gray-700">Tâche</label>
-        <input type="text" id="task" name="task"
-          class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
-      </div>
-      <div>
-        <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
-        <textarea id="description" name="description"
-          class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black"></textarea>
-      </div>
-      <button type="submit" class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded">Ajouter</button>
-    </form>
-  </div>
+<div class="bg-white p-6 rounded shadow w-full">
+  <h3 class="text-xl font-semibold mb-4 text-yellow-500">Ajouter une tâche</h3>
+  <form class="add-task-form space-y-4">
+    <div>
+      <label for="title" class="block text-sm font-medium text-gray-700">Tâche</label>
+      <input type="text" name="title"
+        class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+    </div>
+    <div>
+      <label for="content" class="block text-sm font-medium text-gray-700">Description</label>
+      <textarea name="content"
+        class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black resize-vertical"></textarea>
+    </div>
+    <div>
+      <label for="date" class="block text-sm font-medium text-gray-700">Date d'échéance (laissez vide si pas de date d'échéance)</label>
+      <input type="date" name="date"
+        class="mt-1 block w-full border border-gray-300 rounded py-2 px-3 text-black">
+    </div>
+    <button type="submit" class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded">Ajouter</button>
+  </form>
 </div>
 
 </div>
