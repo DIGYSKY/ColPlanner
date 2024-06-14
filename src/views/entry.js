8a7f187fa22c975f6409a34c0bd7301edc2d472a
Lilyan Chauveau <lilyan.chauveau@gmail.com>
2024-06-14 15:27:53 +0200

    clunup file

--- src/views/entry.js
+++
@@ -1,9 +0,0 @@
-export default () => (`
-<div class="input-message">
-  <form id="imput-user">
-    <input type="text" name="message-user" id="message-user" autocomplete="off" autofocus>
-    <input type="submit" value="Envoyer">
-  </form>
-</div>
-</div>
-`);
--- src/views/message.js
+++
@@ -1,5 +0,0 @@
-export default () => (`
-<div class="message-box">
-<div id="message-box" class="message">
-</div>
-`);
