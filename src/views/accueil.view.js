571e9a04d733b6d01bb9268779dfe012d7d020af
Lilyan Chauveau <lilyan.chauveau@gmail.com>
2024-06-16 18:39:49 +0200

    correcte orthographe

--- src/views/accueil.view.js
+++ src/views/accueil.view.js
@@ -9,7 +9,7 @@ export default () => (`
 <main>
 <h2 class="slogan">Pour que votre colocation ne devienne <br> le prochain épisode de Saw, <br><span>mettez de l'ordre avec <br>ColPlanner !</span></h2>
 <img class="bannier-acc" src="${bannier}" alt="">
-<p class="description">Le projet "CalPlanner" consiste en une application destinée à faciliter la gestion des tâches et des dépenses au sein d'une colocation. Les colocataires pourront créer, attribuer et suivre les tâches domestiques, ainsi que saisir et répartir les dépenses communes telles que le loyer et les factures. L'application comprendra également un calendrier partagé pour visualiser les tâches à venir, ainsi qu'un tableau de bord personnalisé pour chaque colocataire.</p>
+<p class="description">Le projet "ColPlanner" consiste en une application destinée à faciliter la gestion des tâches et des dépenses au sein d'une colocation. Les colocataires pourront créer, attribuer et suivre les tâches domestiques, ainsi que saisir et répartir les dépenses communes telles que le loyer et les factures. L'application comprendra également un calendrier partagé pour visualiser les tâches à venir, ainsi qu'un tableau de bord personnalisé pour chaque colocataire.</p>
 <a class="join-acc" href="/login">COMMANCER</a>
 </main>
 `);
