c219ca15484a1c6800cba39e85d33aab05bd813b
Lilyan Chauveau <lilyan.chauveau@gmail.com>
2024-06-16 18:38:26 +0200

    change first day

--- src/controllers/Calandar.js
+++ src/controllers/Calandar.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -12,6 +13,7 @@ const Calandar = class {
     this.user = JSON.parse(coockieManager.getCookie('user'));
     this.currentColoc = this.user.current_coloc ? this.user.current_coloc : false;
     this.eventListeners = [];
+    this.calendar = null;
     this.run(id);
   }
 
@@ -43,8 +45,9 @@ const Calandar = class {
   initializeCalendar() {
     const calendarEl = document.getElementById('calendar');
 
-    const calendar = new Calendar(calendarEl, {
+    this.calendar = new Calendar(calendarEl, {
       plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      firstDay: 1,
       initialView: 'dayGridMonth',
       locale: 'fr',
       headerToolbar: {
@@ -52,29 +55,13 @@ const Calandar = class {
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      events: [
-        {
-          title: 'Événement 1',
-          start: '2024-06-06',
-          end: '2024-06-07',
-          url: 'https://example.com',
-          classNames: ['custom-class'],
-          editable: true,
-          extendedProps: {
-            description: 'Description de l\'événement 1'
-          }
-        },
-        {
-          title: 'Événement 2',
-          start: '2024-06-05',
-          end: '2024-06-07'
-        }
-      ],
+      events: [],
       customButtons: {
         myCustomButton: {
-          text: 'Custom!',
+          text: 'Ajouter un événement',
           click: (event) => {
             event.preventDefault();
+            this.addEventPopup();
           }
         }
       },
@@ -84,21 +71,156 @@ const Calandar = class {
         week: 'Semaine',
         day: 'Jour'
       },
-      themeSystem: 'standard'
+      themeSystem: 'standard',
+      height: '90vh',
+      eventClick: (info) => {
+        this.eventSelected(info);
+      }
     });
 
-    calendar.render();
+    this.calendar.render();
+  }
+
+  async getEvents() {
+    try {
+      const events = await axios.get(`${this.apiLinks}/events/this`, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+
+      if (events.data.length === 0) {
+        this.noEvents();
+      } else {
+        this.addEvents(events.data);
+      }
+    } catch (error) {
+      this.noEvents();
+    }
+  }
+
+  noEvents() {
+    const noEventPopup = document.querySelector('.no-event-popup');
+    const closeNoEventPopup = document.querySelector('.close-no-event-popup');
+    noEventPopup.classList.remove('hidden');
+    noEventPopup.classList.add('flex');
+
+    this.addEventListeners([closeNoEventPopup], 'click', () => {
+      noEventPopup.classList.remove('flex');
+      noEventPopup.classList.add('hidden');
+    });
+  }
+
+  addEvents(events) {
+    events.forEach((event) => {
+      this.calendar.addEvent(event);
+    });
+    this.calendar.refetchEvents();
+  }
+
+  eventSelected(info) {
+    const eventPopup = document.getElementById('event-popup');
+    const eventTitle = document.getElementById('event-title');
+    const eventDescription = document.getElementById('event-description');
+    const closePopup = document.getElementById('close-popup');
+    const deleteEvent = document.querySelector('.delete-event');
+    deleteEvent.dataset.id = info.event.extendedProps.event_id;
+
+    eventTitle.textContent = info.event.title;
+    eventDescription.textContent = info.event.extendedProps.description || 'Pas de description disponible';
+
+    eventPopup.classList.remove('hidden');
+    eventPopup.classList.add('flex');
+
+    this.addEventListeners([closePopup], 'click', () => {
+      eventPopup.classList.remove('flex');
+      eventPopup.classList.add('hidden');
+    });
+
+    this.addEventListeners([deleteEvent], 'click', () => {
+      this.deleteEvent(deleteEvent.dataset.id);
+      eventPopup.classList.remove('flex');
+      eventPopup.classList.add('hidden');
+    });
+  }
+
+  async deleteEvent(id) {
+    await axios.delete(`${this.apiLinks}/events/${id}`, {
+      headers: { 'Api-Key': this.apiKey }
+    });
+    const event = this.calendar.getEventById(id);
+    if (event) {
+      event.remove();
+    }
+    this.getEvents();
+  }
+
+  addEventPopup() {
+    const addEventPopup = document.querySelector('.add-event-popup');
+    const closeAddEventPopup = document.querySelector('.close-add-event-popup');
+    addEventPopup.classList.remove('hidden');
+    addEventPopup.classList.add('flex');
+
+    this.addEventListeners([closeAddEventPopup], 'click', () => {
+      addEventPopup.classList.remove('flex');
+      addEventPopup.classList.add('hidden');
+    });
+
+    this.addEventListeners([addEventPopup], 'submit', async (event) => {
+      event.preventDefault();
+      await this.addEvent(event);
+    });
+  }
+
+  async addEvent(event) {
+    const title = event.target.querySelector('input[name="title"]').value.trim();
+    const description = event.target.querySelector('textarea[name="description"]').value.trim();
+    const start = event.target.querySelector('input[name="start"]').value;
+    const end = event.target.querySelector('input[name="end"]').value;
+    const color = event.target.querySelector('input[name="color"]').value;
+
+    if (title === '' || start === '' || end === '') {
+      return;
+    }
+
+    let request = false;
+
+    try {
+      await axios.post(`${this.apiLinks}/events/this`, {
+        title,
+        description,
+        start,
+        end,
+        color
+      }, {
+        headers: { 'Api-Key': this.apiKey }
+      });
+
+      request = true;
+    } catch (error) {
+      request = false;
+    }
+
+    if (request) {
+      this.getEvents();
+
+      const addEventPopup = document.querySelector('.add-event-popup');
+
+      addEventPopup.classList.remove('flex');
+      addEventPopup.classList.add('hidden');
+    }
   }
 
   addEventListeners(elements, event, callback) {
     elements.forEach((element) => {
-      element.addEventListener(event, callback);
-      this.eventListeners.push({ element, event, callback });
+      if (element) {
+        element.addEventListener(event, callback);
+        this.eventListeners.push({ element, event, callback });
+      }
     });
   }
 
-  run(id) {
-    this.renderCalandar(id);
+  async run(id) {
+    await this.renderCalandar(id);
+    await this.getEvents();
   }
 
   destroy() {
